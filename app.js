d32bcbbef2a37ad5d5c2f38e35ad782d07f3f523
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-04-09 18:11:54 +0530

    Added Route for Employee

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express();
 // Setting server port
 const port = process.env.PORT || 3000;
 
+// importing routes
+const employeeRouter = require('./src/routes/employee.routes');
+
+
 // Parse Content (as body parser)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +20,8 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
+// Using employee router
+app.use('/employee', employeeRouter);
 
 
 app.listen(port, () => {
